5bd8e3e9ca52b87fb9ad2f288be98f9a0b761261
abdullah1shah <shah.145abdullah1@gmail.com>
2025-01-07 11:01:36 +0500

    Your commit message here

--- lab9/features/steps/password_reset_steps.py
+++ lab9/features/steps/password_reset_steps.py
@@ -1,5 +1,4 @@
-# features/steps/password_reset_steps.py
-from behave import given, when, then
+from behave import when, then
 
 @when('I enter my registered email')
 def step_impl(context):
@@ -11,13 +10,19 @@ def step_impl(context):
     print("Entered unregistered email")
     context.email_valid = False
 
+@when('I click reset password button')
+def step_impl(context):
+    print("Clicked reset password button")
+    context.reset_button_clicked = True
+
 @then('I should receive a reset link')
 def step_impl(context):
-    assert context.email_valid
-    print("Reset link sent")
+    assert context.email_valid and context.reset_button_clicked
+    print("Reset link sent to registered email")
 
 @then('I should see a confirmation message')
 def step_impl(context):
+    assert context.email_valid and context.reset_button_clicked
     print("Confirmation message displayed")
 
 @then('I should not receive a reset link')
